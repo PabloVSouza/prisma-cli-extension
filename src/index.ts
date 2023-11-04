5328bde7ced5f68319fd485519899726efda98f6
Pablo Souza <30412188+PabloVSouza@users.noreply.github.com>
2023-11-04 01:47:10 +0000

    Move files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
 /// <reference types="node" />
 
-export * from './lib/index'
+//@ts-ignore
+import { PrismaClient } from '../../../../node_modules/@prisma/client'
+import { PrismaMigration } from './migration'
+
+export class PrismaInitializer extends PrismaMigration {
+  public prisma: PrismaClient
+
+  constructor(public dbUrl: string, public latestMigration: string) {
+    super(dbUrl, latestMigration)
+    this.prisma = this.initializePrisma()
+  }
+
+  private initializePrisma = (): PrismaClient => {
+    return new PrismaClient({
+      datasources: {
+        db: {
+          url: this.dbUrl
+        }
+      },
+      //@ts-ignore
+      __internal: {
+        engine: {
+          binaryPath: this.qePath
+        }
+      }
+    })
+  }
+}
